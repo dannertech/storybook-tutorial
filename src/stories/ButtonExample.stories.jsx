4839d450ba580156a9ad54dd8938505a22a3497d
dannertech <danner@danner.tech>
2023-03-22 22:22:37 -0400

    created Storybook test

---
+++ src/stories/ButtonExample.stories.jsx
@@ -0,0 +1,13 @@
+
+import {Button} from './Button.jsx'
+
+export default {
+    title: 'Button',
+    component: Button
+};
+
+export const Red = () => {
+    return (
+        <Button backgroundColor='red' label="Press Me"/>
+    )
+};
